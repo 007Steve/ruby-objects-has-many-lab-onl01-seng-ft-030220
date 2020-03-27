b025532654be0242e73848b2c6fb5f06aedb3403
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-03-27 17:02:17 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,10 @@
+class Artist 
+  attr_accessor :name,:songs
+  
+  def initialize(name,songs)
+  @name = name
+  @songs = songs
+end
+
+
+end 
---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Artist 
+  attr_accessor :name,:songs
+  
+  def initialize(name,songs)
+  @name = name
+  @songs = songs
+end
+
+def add_songs
+  
+end
+end 
